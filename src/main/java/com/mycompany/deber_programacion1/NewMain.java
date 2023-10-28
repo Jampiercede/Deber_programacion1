9e566f25403ab0142d4a11fe5a518c13926b51ef
Jampier <jcedeno3555@utm.edu.ec>
2023-10-28 09:15:33 -0500

    Commit 1: Importaciones y declaración de clase

---
+++ src/main/java/com/mycompany/deber_programacion1/NewMain.java
@@ -0,0 +1,14 @@
+// Commit 1: Agregar estructura básica y clases de utilidad
+
+package com.mycompany.deber_programacion1;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class NewMain {
+    
+    public static void main(String[] args) {
+        // Aquí va el código
+    }
+}
+
