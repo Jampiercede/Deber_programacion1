5d2a1040aecb0b12f71752bff5f7803ed8542972
Jampier <jcedeno3555@utm.edu.ec>
2023-10-28 09:58:30 -0500

    Commit 2

--- src/main/java/com/mycompany/deber_programacion1/QuestionGenerator.java
+++ src/main/java/com/mycompany/deber_programacion1/QuestionGenerator.java
@@ -1,6 +1,30 @@
 package com.mycompany.deber_programacion1;
+import java.security.SecureRandom;
 
 public class QuestionGenerator {
-    // Métodos para generar preguntas
+    
+    private SecureRandom random = new SecureRandom();
+    
+    public String generateQuestion(int difficulty, int operationType) {
+        int operand1 = getRandomNumber(difficulty);
+        int operand2 = getRandomNumber(difficulty);
+        switch (operationType) {
+            case 1:
+                return "¿Cuánto es " + operand1 + " más " + operand2 + "?";
+            case 2:
+                return "¿Cuánto es " + operand1 + " menos " + operand2 + "?";
+            case 3:
+                return "¿Cuánto es " + operand1 + " por " + operand2 + "?";
+            case 4:
+                return "¿Cuánto es " + operand1 + " dividido " + operand2 + "?";
+            default:
+                return "Operación no soportada";
+        }
+    }
+    
+    private int getRandomNumber(int difficulty) {
+        int bound = (int) Math.pow(10, difficulty) - 1;
+        return random.nextInt(bound) + 1;
+    }
 }
 
