7387b44dae4984d7a07a32018cabe52fd5c61c8e
Jampier <jcedeno3555@utm.edu.ec>
2023-10-28 09:59:44 -0500

    Commit 3

--- src/main/java/com/mycompany/deber_programacion1/UserInterface.java
+++ src/main/java/com/mycompany/deber_programacion1/UserInterface.java
@@ -1,6 +1,31 @@
 package com.mycompany.deber_programacion1;
+import java.util.Scanner;
 
 public class UserInterface {
-    // Métodos para interactuar con el usuario
+    
+    private Scanner scanner = new Scanner(System.in);
+    
+    public int askDifficulty() {
+        System.out.println("Elige el nivel de dificultad (1: fácil, 2: medio, etc.): ");
+        return scanner.nextInt();
+    }
+    
+    public int askOperationType() {
+        System.out.println("Elige el tipo de operación (1: suma, 2: resta, 3: multiplicación, 4: división): ");
+        return scanner.nextInt();
+    }
+    
+    public int getUserAnswer() {
+        System.out.println("Ingresa tu respuesta: ");
+        return scanner.nextInt();
+    }
+    
+    public void showQuestion(String question) {
+        System.out.println(question);
+    }
+    
+    public void showFeedback(String feedback) {
+        System.out.println(feedback);
+    }
 }
 
